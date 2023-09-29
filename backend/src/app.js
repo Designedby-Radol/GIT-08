ecaf35e054c88ce73f1a10380cd7d849e428bcb7
Designedby-Radol <raulsantiagoramirez1@gmail.com>
2023-09-28 22:07:20 -0500

    N5

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,3 +1,5 @@
+require('dotenv').config()
+require('./db')
 //llamar las librerias
 const express = require(`express`)
 
--- backend/src/db.js
+++ backend/src/db.js
@@ -1,10 +1,10 @@
 const mongoose = require(`mongoose`)
-const bdpato = process.env.BDPATO
+//const bdpato = process.env.BDPATO
 
 const uri = process.env.URI
-const atluri= `${process.env.URI}/${bdpato}`
+//"mongodb+srv://raulsantigoramirez:1031648361r@radol.q4e4eeh.mongodb.net/bit01?retryWrites=true&w=majority"
 
-mongoose.connect(atluri,{useUnifiedTopology: true, useNewUrlParser: true})
+mongoose.connect(uri,{useUnifiedTopology: true, useNewUrlParser: true})
 .then(() => console.log("conectado a DB"))
 .catch((error) => console.log(error))
 
